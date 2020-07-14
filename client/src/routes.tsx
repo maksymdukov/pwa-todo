789e399dbce7564bf3791a3a21a4b5553837190f
Maksym Dukov <maxopowerful@gmail.com>
2020-07-14 16:56:33 +0300

    Client: - refactor routing

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -4,67 +4,34 @@ import { SignIn } from "pages/auth/signin";
 import { useSelector } from "react-redux";
 import { getIsAuthenticated } from "store/user/selectors";
 import { AuthRoutes } from "./auth-routes";
-import { ProtectedRoute } from "components/route";
 import SignUp from "pages/auth/signup";
 import ActivateAccount from "pages/auth/activate-account";
 import ResetPasswordStart from "pages/auth/reset-password-start";
 import ResetPasswordFinished from "pages/auth/reset-password-finish";
 import { useServiceWorkerEvents } from "hooks/use-service-worker-events";
 
+const routes = [
+  { path: "/signin", exact: true, component: SignIn },
+  { path: "/signup", exact: true, component: SignUp },
+  { path: "/activateaccount", exact: true, component: ActivateAccount },
+  { path: "/resetpassword", exact: true, component: ResetPasswordStart },
+  {
+    path: "/resetpasswordfinish",
+    exact: true,
+    component: ResetPasswordFinished,
+  },
+];
+
 export const Routes = () => {
   const isAuthenticated = useSelector(getIsAuthenticated);
   useServiceWorkerEvents();
-  // TODO refactor routes to avoid code duplications
   return (
     <Switch>
-      <Route
-        path="/signin"
-        exact
-        render={(props) =>
-          isAuthenticated ? <Redirect to="/todos" /> : <SignIn {...props} />
-        }
-      />
-      <Route
-        path="/signup"
-        exact
-        render={(props) =>
-          isAuthenticated ? <Redirect to="/todos" /> : <SignUp {...props} />
-        }
-      />
-      <Route
-        path="/activateaccount"
-        exact
-        render={(props) =>
-          isAuthenticated ? (
-            <Redirect to="/todos" />
-          ) : (
-            <ActivateAccount {...props} />
-          )
-        }
-      />
-      <Route
-        path="/resetpassword"
-        exact
-        render={(props) =>
-          isAuthenticated ? (
-            <Redirect to="/todos" />
-          ) : (
-            <ResetPasswordStart {...props} />
-          )
-        }
-      />
-      <Route
-        path="/resetpasswordfinish"
-        exact
-        render={(props) =>
-          isAuthenticated ? (
-            <Redirect to="/todos" />
-          ) : (
-            <ResetPasswordFinished {...props} />
-          )
-        }
-      />
-      <ProtectedRoute component={AuthRoutes} />
+      {isAuthenticated && <AuthRoutes />}
+      {routes.map((route) => (
+        <Route key={route.path} {...route} />
+      ))}
+      <Redirect to="/signin" />
     </Switch>
   );
 };
